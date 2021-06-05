bc2aa0c45b38e55a7b4111c5726c40cf93f5fddf
RoheyJohn <johnrohey@gmail.com>
2021-06-05 10:13:50 +0000

    1 file modified

--- src/Search.js
+++ src/Search.js
@@ -2,18 +2,37 @@ import React from "react";
 import './Search.css';
 import SearchIcon from "@material-ui/icons/search";
 
-export default function Search() {
+export default function Search({ hideButtons = false }) {
+
+  const [input, setInput] = useState("");
+  const history = useHistory();
+
+  const search = (e) => {
+    e.preventDefault();
+    console.log('you hide the search button', input)
+
+    history.push('/search')
+  }
   return (
-    
     <form className="search">
       <div className="search__input">
         <SearchIcon className="search__inputIcon" />
         <input />
       </div>
-      
-      <div className="search__buttons">
-      
+
+      {!hideButtons ? (
+        <div className="search__buttons">
+        <Buttons type="submit" onClick={search} variants="outlined">Constitution Search</Buttons>
+        <Buttons variants="outlined">Favorites</Buttons>
       </div>
+      ) : (
+        <div className="search__buttons">
+        <Buttons className="search__buttonsHidden" type="submit" onClick={search} variants="outlined">Constitution Search</Buttons>
+        <Buttons className="search__buttonsHidden" variants="outlined">Favorites</Buttons>
+      </div>
+      )}
+      
+      
     </form>
   );
 }
